7f32c7e2c3702f9716d567a8fd010f29b7082838
kwak914 <kwak914@gmail.com>
2017-08-09 16:37:34 +0900

    Update reduce_token_3_v2.py

--- parse/reduce_token_3_v2.py
+++ parse/reduce_token_3_v2.py
@@ -1,5 +1,4 @@
 
-
 # -*- coding: utf-8 -*-
 
 import konlpy
@@ -14,40 +13,17 @@ import math
 from collections import Counter
 import sys
 
-DATA_PATH = "../real_data/"
-#CLIENT_DATA_PATH = DATA_PATH + 'client/'
-#RESPONSE_DATA_PATH = DATA_PATH + 'response/'
-CLIENT_DATA_PATH = DATA_PATH + 'new_cli/'
-RESPONSE_DATA_PATH = DATA_PATH + 'new_res/'
-
-MAX_SENTENCE_LENGTH = 160
-TOPIC_NUM =5
-
-# POS tag a sentence
-word_list = []
-word_dic = {}
-word_indices = []
-word_indexing = {}
-whole_document = []
-
-
-
-word_count = 0
-texts = []
-y = []
-delete_list = []
-
 
 document_dict = {}
-tfidf_dic = {}
-document_index = 0
-
-doc_name = ''
+word_frequency = {}
+useless_wordlist = []
 
 
+with open('word_dic', 'rb') as handle:
+   word_frequency= pickle.load(handle)
+   
 with open('document_dict1_revised', 'rb') as handle:
    document_dict= pickle.load(handle)
-#print (document_dict)
 with open('document_dict2_revised', 'rb') as handle:
    document_dict.update(pickle.load(handle))
 with open('document_dict3_revised', 'rb') as handle:
@@ -56,16 +32,17 @@ with open('document_dict4_revised', 'rb') as handle:
    document_dict.update(pickle.load(handle))
 
 document_dict = {k:v for k,v in document_dict.items() if v!= []}
-#print (document_dict)
         
-pickling = document_dict
 with open('reduced_document_dict', 'wb') as handle:
-    pickle.dump(pickling, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(document_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-        
 for word in useless_wordlist:
     try :
         del word_frequency[word]
     except :
         # print( "KEY ERROR in removing '%s' from word frequency dict" %(word) )
         continue
+
+with open('word_dic', 'wb') as handle:
+    pickle.dump(word_frequency, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
