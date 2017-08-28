bb4d160533f61e2a394efce5c35ab6906e07c7aa
kwak914 <kwak914@gmail.com>
2017-08-28 11:46:59 +0900

    Update split_topic_paragraph.py

--- keyword/split_topic_paragraph.py
+++ keyword/split_topic_paragraph.py
@@ -18,16 +18,14 @@ PICKLE_DATA_PATH = './pickle/'
 
 TEST_DOC = 50000
 
-
+with open(PICKLE_DATA_PATH+'paragraph_topic', 'rb') as handle:
+    document_paragraph_topic = pickle.load(handle)
+    
 with open(PICKLE_DATA_PATH+'reduced_document_paragraph2', 'rb') as handle:
     reduced_document_dict_paragraph = pickle.load(handle)
 
 with open(PICKLE_DATA_PATH+'before_bayes_paragraph2', 'rb') as handle:
     before_bayes_paragraph = pickle.load(handle)
-
-print( reduced_document_dict_paragraph[205])
-print()
-print( before_bayes_paragraph[205])
 
 
 with open(PICKLE_DATA_PATH+'topic_classifier_1st', 'rb') as handle:
@@ -36,35 +34,20 @@ with open(PICKLE_DATA_PATH+'topic_classifier_1st', 'rb') as handle:
 with open(PICKLE_DATA_PATH+'topic_classifier_2nd', 'rb') as handle:
     topic_bag_1v2 = pickle.load(handle)
 
-document_paragraph_topic = {}
+paragraph_classifier= {}
 
-for doc_idx in reduced_document_dict_paragraph:
-    if(TEST_DOC < doc_idx):
-        break
-    document_paragraph_topic[doc_idx] = {}
-    for par_idx in range(len(reduced_document_dict_paragraph[doc_idx])):
-        temp_list = reduced_document_dict_paragraph[doc_idx][par_idx]
-        document_paragraph_topic[doc_idx][par_idx] = []
-        for main in topic_bag_1v1:
-            if(len(list(set(temp_list) - set(topic_bag_1v1[main]))) < len(list(set(temp_list)))):
-                document_paragraph_topic[doc_idx][par_idx].append(main)
+for main in topic_bag_1v1:
+    paragraph_classifier[main] = []
 
-'''
-for doc_idx in reduced_document_dict_paragraph:
-    if(TEST_DOC < doc_idx):
-        break
-    for par_idx in range(len(reduced_document_dict_paragraph[doc_idx])):
-        if (len(document_paragraph_topic[doc_idx][par_idx])>0):
-            continue
-        temp_list = reduced_document_dict_paragraph[doc_idx][par_idx]
-        for main in topic_bag_1v2:
-            if(len(list(set(temp_list) - set(topic_bag_1v1[main]))) < len(list(set(temp_list)))-3):
-                document_paragraph_topic[doc_idx][par_idx].append('2nd_'+main)
-'''
-                
 for doc_idx in document_paragraph_topic:
-    print( document_paragraph_topic[doc_idx])
+     if(TEST_DOC < doc_idx):
+        break
+     for par_idx in document_paragraph_topic[doc_idx]:
+        for main in topic_bag_1v1:
+            if (main in document_paragraph_topic[doc_idx][par_idx]):
+                paragraph_classifier.append(reduced_document_dict_paragraph[doc_idx][par_idx])
+            
+
     
-    
-with open(PICKLE_DATA_PATH+'paragraph_topic', 'wb') as handle:
-    pickle.dump(document_paragraph_topic, handle, protocol=pickle.HIGHEST_PROTOCOL)
+with open(PICKLE_DATA_PATH+'paragraph_classifier', 'wb') as handle:
+    pickle.dump(paragraph_classifier, handle, protocol=pickle.HIGHEST_PROTOCOL)
