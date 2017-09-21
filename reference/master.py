03c3b74c1f8a4b4f42374cd76712266642520d88
kwak914 <kwak914@gmail.com>
2017-09-21 15:52:24 +0900

    Update master.py

--- reference/master.py
+++ reference/master.py
@@ -13,17 +13,23 @@ def sub_process_preprocessing(process_num):
     #preprocessing_advanced.py should be changed to recognize batch_num
     # example : result = subprocess.check_output('python3 preprocessing_advanced.py ' + str(process_num), shell=True)
     result = subprocess.check_output('python3 preprocessing_advanced.py', shell=True)
-if __name__ == '__main_':
-    print ("this is tool for debugging master.py code")
-    print ()
-    print ("##########################################")
+    print ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print ("process num %d"%process_num)
+    result = str(result, "utf-8")
+    print (result)
+
     
-    #read_sample_data could be relpaced to data_helpers.py , to-be
-    #sample_data, sample_data_path = preprocessing.read_sample_data()
-    pool = multiprocessing.Pool(processes = CPU_USAGE)
-    pool.map(sub_process_preprocessing, range(0,CPU_USAGE))
-    
-    print ("joined sub_processes")
-    
-    
-    
+####### master code starts ##########
+
+print ("this is tool for debugging master.py code")
+print ()
+print ("##########################################")
+
+#read_sample_data could be relpaced to data_helpers.py , to-be
+#sample_data, sample_data_path = preprocessing.read_sample_data()
+pool = multiprocessing.Pool(processes = CPU_USAGE)
+pool.map(sub_process_preprocessing, range(0,CPU_USAGE))
+
+print ("joined sub_processes")
+   
+  
