aa6558b52570390ef9c5b6fe3a666fbab2aef482
kwak914 <kwak914@gmail.com>
2017-09-21 15:42:24 +0900

    Update master.py

--- reference/master.py
+++ reference/master.py
@@ -4,13 +4,15 @@ import re
 import subprocess
 #should be finalized as "import preprocessing"
 import preprocessing_advanced as preprocessing
+import multiprocessing
 
 DATA_PATH = 'sample_data'
+CPU_USAGE = 4
 
 def sub_process_preprocessing(process_num):
     #preprocessing_advanced.py should be changed to recognize batch_num
-    result = subprocess.check_output('python3 preprocessing_advanced')
-
+    # example : result = subprocess.check_output('python3 preprocessing_advanced.py ' + str(process_num), shell=True)
+    result = subprocess.check_output('python3 preprocessing_advanced.py', shell=True)
 if __name__ == '__main_':
     print ("this is tool for debugging master.py code")
     print ()
@@ -18,5 +20,10 @@ if __name__ == '__main_':
     
     #read_sample_data could be relpaced to data_helpers.py , to-be
     sample_data, sample_data_path = preprocessing.read_sample_data()
+    pool = multiprocessing.Pool(processes = CPU_USAGE)
+    pool.map(sub_process_preprocessing, range(0,CPU_USAGE))
+    
+    print ("joined sub_processes")
+    
     
     
